216ed4096fcd66d68b0122daa7a77aa2fda83f09
SimonovaDari <d.poddubnaya@gmail.com>
2020-10-07 14:25:52 -0700

    Minor change related to Math.Random

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Column from "./Column";
 import 'bootstrap/dist/css/bootstrap.css';
 import Controller from "./Controller";
 
+function getId() {
+    return Math.random();
+}
+
 const taskArray = [
-    {id: Math.random(), name: 'First Task', status: 'todo'},
-    {id: Math.random(), name: 'Second Task', status: 'progress'},
-    {id: Math.random(), name: 'Third Task', status: 'review'},
-    {id: Math.random(), name: 'Fourth Task', status: 'done'},
-    {id: Math.random(), name: 'Fifth Task', status: 'progress'},
-    {id: Math.random(), name: 'Sixth Task', status: 'done'},
-    {id: Math.random(), name: 'Seventh Task', status: 'review'},
+    {id: getId(), name: 'First Task', status: 'todo'},
+    {id: getId(), name: 'Second Task', status: 'progress'},
+    {id: getId(), name: 'Third Task', status: 'review'},
+    {id: getId(), name: 'Fourth Task', status: 'done'},
+    {id: getId(), name: 'Fifth Task', status: 'progress'},
+    {id: getId(), name: 'Sixth Task', status: 'done'},
+    {id: getId(), name: 'Seventh Task', status: 'review'},
 ];
 const columnArray = [
-    {id: Math.random(), title: 'To Do', status: 'todo'},
-    {id: Math.random(), title: 'Progress', status: 'progress'},
-    {id: Math.random(), title: 'Review', status: 'review'},
-    {id: Math.random(), title: 'Done', status: 'done'},
+    {id: getId(), title: 'To Do', status: 'todo'},
+    {id: getId(), title: 'Progress', status: 'progress'},
+    {id: getId(), title: 'Review', status: 'review'},
+    {id: getId(), title: 'Done', status: 'done'},
 ];
 
 const statuses = ['todo', 'progress', 'review', 'done'];
@@ -25,7 +29,7 @@ function App() {
     const [tasks, setTasks] = useState(taskArray)
 
     const createTask = (newName, newStatus) => {
-        const newTask = [...tasks, {id: Math.random(), name: newName, status: newStatus}];
+        const newTask = [...tasks, {id: getId(), name: newName, status: newStatus}];
         setTasks(newTask)
     }
 
